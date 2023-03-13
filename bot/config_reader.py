dd4dee51409146bb62abbbb87e5228f1c041811e
Groosha <groosha@protonmail.com>
2023-03-14 00:40:50 +0800

    Added Redis to config

--- bot/config_reader.py
+++ bot/config_reader.py
@@ -1,10 +1,11 @@
-from pydantic import BaseSettings, SecretStr
+from pydantic import BaseSettings, SecretStr, RedisDsn
 
 
 class Settings(BaseSettings):
     bot_token: SecretStr
     forum_supergroup_id: int
     remove_sent_confirmation: bool
+    redis_dsn: RedisDsn
 
     class Config:
         env_file = '.env'
