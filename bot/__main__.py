f89e52db58a7a2ae2a37210553c6f43bf5680613
Groosha <groosha@protonmail.com>
2023-04-05 00:22:13 +0800

    Updated main file

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,33 +1,32 @@
 import asyncio
 
+import motor.motor_asyncio
 import structlog
 from aiogram import Bot, Dispatcher
-from cachetools import LRUCache
-from redis.asyncio import Redis
+# from cachetools import LRUCache
 
 from bot.config_reader import config
-from bot.handlers import basic_commands, from_users
-from bot.middlewares import TopicsMiddleware, BansMiddleware
+from bot.handlers import basic_commands, from_users, from_forum
+from bot.middlewares import TopicsManagementMiddleware
 
 log: structlog.BoundLogger = structlog.get_logger()
 
 
 async def main():
-    redis_connection = Redis.from_url(config.redis_dsn)
-    # Raises ConnectionError, if Redis is not reachable
-    await redis_connection.ping()
+    mongodb_client = motor.motor_asyncio.AsyncIOMotorClient("localhost", 27017, username="user", password="password", authSource='bot')
+    mongodb_connection = mongodb_client.bot
 
-    local_cache = LRUCache(maxsize=5000)
-    bans_cache = LRUCache(maxsize=5000)
-    shadowbans_cache = LRUCache(maxsize=5000)
+    # bans_cache = LRUCache(maxsize=5000)
+    # shadowbans_cache = LRUCache(maxsize=5000)
 
     bot = Bot(token=config.bot_token.get_secret_value())
-    dp = Dispatcher(forum_chat_id=config.forum_supergroup_id)
+    dp = Dispatcher(forum_chat_id=config.forum_supergroup_id, mongo=mongodb_connection)
 
-    from_users.router.message.middleware(BansMiddleware(redis_connection, bans_cache, shadowbans_cache))
-    from_users.router.message.middleware(TopicsMiddleware(redis_connection, local_cache))
+    # from_users.router.message.middleware(BansMiddleware(redis_connection, bans_cache, shadowbans_cache))
+    dp.message.outer_middleware(TopicsManagementMiddleware(mongodb_connection))
     dp.include_router(basic_commands.router)
     dp.include_router(from_users.router)
+    dp.include_router(from_forum.router)
 
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
