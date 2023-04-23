441b5fdfe0001eaaa43e971c2c339a4e587c3607
Groosha <groosha@protonmail.com>
2023-04-24 00:02:28 +0800

    Updated env_example and config_reader.py

--- bot/config_reader.py
+++ bot/config_reader.py
@@ -1,15 +1,31 @@
-from pydantic import BaseSettings, SecretStr, MongoDsn
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseSettings, SecretStr, RedisDsn, PostgresDsn, BaseModel
+
+
+class FSMModeEnum(str, Enum):
+    MEMORY = "memory"
+    REDIS = "redis"
+
+
+class Redis(BaseModel):
+    dsn: RedisDsn  # Without database id!
+    fsm_db_id: int
+    data_db_id: int
 
 
 class Settings(BaseSettings):
     bot_token: SecretStr
     forum_supergroup_id: int
-    remove_sent_confirmation: bool
-    mongo_dsn: MongoDsn
+    redis: Optional[Redis]
+    postgres_dsn: PostgresDsn
+    fsm_mode: FSMModeEnum
 
     class Config:
         env_file = '.env'
         env_file_encoding = 'utf-8'
+        env_nested_delimiter = '__'
 
 
 config = Settings()
