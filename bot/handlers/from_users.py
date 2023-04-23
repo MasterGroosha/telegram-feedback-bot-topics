9031672e44f4a8a193ea0d6864adfb8e8e5f0ab0
Groosha <groosha@protonmail.com>
2023-04-24 00:03:59 +0800

    Added separate /start handler

--- bot/handlers/from_users.py
+++ bot/handlers/from_users.py
@@ -1,30 +1,27 @@
 import structlog
 from aiogram import Router, F
-from aiogram.filters import Command
+from aiogram.filters import CommandStart
 from aiogram.types import Message
 
 from bot.config_reader import config
 
-router = Router(name="From Users Router")
+router = Router(name="Users router")
 log: structlog.BoundLogger = structlog.get_logger()
 
 # This router should only work in PM
 router.message.filter(F.chat.type == "private")
 
 
-@router.message(Command("start"))
+@router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer("% Start command text %")
+    await message.answer("% start command message %")
 
 
 @router.message()
 async def any_message(message: Message, topic_id: int, reply_to_id: int | None = None):
-    print(f"Replying to {reply_to_id}")
     msg: Message = await message.send_copy(
         chat_id=config.forum_supergroup_id,
         message_thread_id=topic_id,
         reply_to_message_id=reply_to_id
     )
     return msg
-
-
