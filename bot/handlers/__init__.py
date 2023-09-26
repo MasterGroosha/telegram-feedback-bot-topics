17f42b020fefe94f74dfd4958994291e398afa57
Groosha <groosha@protonmail.com>
2023-09-26 19:54:23 +0800

    Re-introduce separate handler for actions in PM with bot

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -6,6 +6,7 @@ from bot.middlewares import (
     MessageConnectionsMiddleware, EditedMessagesMiddleware
 )
 from . import actions_in_forum
+from . import actions_in_pm
 from . import message_edits
 from . import transfer_messages
 
@@ -14,6 +15,7 @@ def get_router(bot_config: BotSettings) -> Router:
     main_router = Router(name="all_updates_router")
 
     actions_in_forum.router.message.filter(F.chat.id == bot_config.forum_supergroup_id)
+    actions_in_pm.router.message.filter(F.chat.type == "private")
 
     if bot_config.albums_preserve_enabled:
         transfer_messages.router.message.outer_middleware(
@@ -32,6 +34,7 @@ def get_router(bot_config: BotSettings) -> Router:
     # Attach other routers to main router
     main_router.include_routers(
         actions_in_forum.router,
+        actions_in_pm.router,
         transfer_messages.router,
         message_edits.router
     )
