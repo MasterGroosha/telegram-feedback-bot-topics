93888bfd1694f1493d2402cdaae4e5fa90fb80cc
Groosha <groosha@protonmail.com>
2023-10-03 21:41:02 +0800

    Fix getting database DSN

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,7 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from bot.config_reader import config as bot_config
+from bot.config_reader import Settings, parse_settings
 from bot.db import Base
 
 # this is the Alembic Config object, which provides
@@ -23,10 +23,11 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+settings: Settings = parse_settings()
 target_metadata = Base.metadata
 config.set_main_option(
     'sqlalchemy.url',
-    bot_config.postgres_dsn
+    str(settings.postgres.dsn)
 )
 
 # other values from the config, defined by the needs of env.py,
