b610cd9c034bf0bb556add28f72f2182bee58e33
Groosha <groosha@protonmail.com>
2023-10-07 16:23:02 +0800

    Ban and shadowban stubs; started working on feature

--- bot/handlers/actions_in_forum.py
+++ bot/handlers/actions_in_forum.py
@@ -16,3 +16,13 @@ async def note(message: Message):
     :param message: any message which text or caption starts with "!note"
     """
     return
+
+
+@router.message(Command("ban"))
+async def cmd_ban(message: Message):
+    pass
+
+
+@router.message(Command("shadowban"))
+async def cmd_shadowban(message: Message):
+    pass
