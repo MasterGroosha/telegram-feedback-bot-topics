4a29fa9e745a152d554562b6cfc9263abad96d84
Groosha <groosha@protonmail.com>
2023-10-15 22:36:43 +0800

    Added UserTopicContextMiddleware

--- bot/middlewares/__init__.py
+++ bot/middlewares/__init__.py
@@ -1,5 +1,6 @@
 from .albums_collector import AlbumsMiddleware
 from .bans import BansMiddleware
+from .context import UserTopicContextMiddleware
 from .db import DbSessionMiddleware
 from .edited_messages import EditedMessagesMiddleware
 from .message_connections import MessageConnectionsMiddleware
@@ -8,6 +9,7 @@ from .topics_management import TopicsManagementMiddleware
 __all__ = [
     "AlbumsMiddleware",
     "BansMiddleware",
+    "UserTopicContextMiddleware",
     "DbSessionMiddleware",
     "EditedMessagesMiddleware",
     "MessageConnectionsMiddleware",
