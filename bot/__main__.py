8429d466cc633f6481f57814dc38e78660c9dd95
Groosha <groosha@protonmail.com>
2024-12-01 18:41:43 +0100

    Some fixes

--- bot/__main__.py
+++ bot/__main__.py
@@ -39,5 +39,4 @@ async def main():
     await dp.start_polling(bot)
 
 
-if __name__ == '__main__':
-    asyncio.run(main())
+asyncio.run(main())
--- bot/db/__init__.py
+++ bot/db/__init__.py
@@ -1,5 +1,8 @@
 from .base import Base
+from .models import MessageConnection, Topic
 
 __all__ = [
     "Base",
+    "MessageConnection",
+    "Topic",
 ]
