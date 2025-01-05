382203ad95e22534423213114deb90144aa43261
Groosha <groosha@protonmail.com>
2025-01-05 21:52:36 +0100

    Removed unnecessary handler

--- bot/handlers/group_commands.py
+++ bot/handlers/group_commands.py
@@ -1,10 +1,3 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
-from aiogram.types import Message
 
 router = Router()
-
-
-@router.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer("Command /start in Group")
